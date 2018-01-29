f09d5c6fc0afc56a4271ff9e03e466162c41c3e8
Gustavo <gustavo.vh@gmail.com>
2018-01-29 16:04:26 -0500

    Decision trees mini-project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """ 
-    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+    This is the code to accompany the Lesson 4 (decision tree) mini-project.
 
     Use a Decision Tree to identify emails from the Enron corpus by author:    
     Sara has label 0
@@ -24,8 +24,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+'''
+L4Q37
 
+Get a decision tree up and running as a classifier, setting 
+min_samples_split=40. It will probably take a while to train. What’s the 
+accuracy?
+'''
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print 'Accracy with min_samples_split=40: ', acc
+
+'''
+L4Q38
+
+What's the number of features in your data? (Hint: the data is organized into 
+a numpy array where the number of rows is the number of data points and the 
+number of columns is the number of features; so to extract this number, 
+use a line of code like len(features_train[0]).)
+'''
+print 'Number of features: ',len(features_train[0])
 #########################################################
 
 
